9881903452589e9a6b48ea7fa4834243c1e496c4
Will Brown <willbrown@wills-mbp.o3international.local>
2018-04-03 10:33:01 -0700

    components added for want to read, currently reading, and read

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
-import Shelf from './components/Shelves.js'
+import Shelf from './components/Shelf.js'
 
 class BooksApp extends React.Component {
   state = {
@@ -17,7 +17,7 @@ class BooksApp extends React.Component {
 
   render() {
 
-    const wantToReadBooks = {
+    const currentlyReadingBooks = {
                   "books":
                     [{
                         "title": "The Linux Command Line",
@@ -110,8 +110,154 @@ class BooksApp extends React.Component {
                         "id": "sJf1vQAACAAJ",
                         "shelf": "currentlyReading"
                     }
-]
+                  ]
                 }
+
+    const wantToReadBooks = {
+                      "books": [
+                {
+                "title": "The Cuckoo's Calling",
+                "authors": [
+                    "Robert Galbraith"
+                ],
+                "publisher": "Mulholland Books",
+                "publishedDate": "2013-04-30",
+                "description": "A brilliant debut mystery in a classic vein: Detective Cormoran Strike investigates a supermodel's suicide. After losing his leg to a land mine in Afghanistan, Cormoran Strike is barely scraping by as a private investigator. Strike is down to one client, and creditors are calling. He has also just broken up with his longtime girlfriend and is living in his office. Then John Bristow walks through his door with an amazing story: His sister, thelegendary supermodel Lula Landry, known to her friends as the Cuckoo, famously fell to her death a few months earlier. The police ruled it a suicide, but John refuses to believe that. The case plunges Strike into the world of multimillionaire beauties, rock-star boyfriends, and desperate designers, and it introduces him to every variety of pleasure, enticement, seduction, and delusion known to man. You may think you know detectives, but you've never met one quite like Strike. You may think you know about the wealthy and famous, but you've never seen them under an investigation like this. Introducing Cormoran Strike, this is the acclaimed first crime novel by J.K. Rowling, writing under the pseudonym Robert Galbraith.",
+                "industryIdentifiers": [
+                    {
+                        "type": "ISBN_13",
+                        "identifier": "9780316206860"
+                    },
+                    {
+                        "type": "ISBN_10",
+                        "identifier": "0316206865"
+                    }
+                ],
+                "readingModes": {
+                    "text": true,
+                    "image": false
+                },
+                "pageCount": 464,
+                "printType": "BOOK",
+                "categories": [
+                    "Fiction"
+                ],
+                "averageRating": 3.5,
+                "ratingsCount": 3037,
+                "maturityRating": "NOT_MATURE",
+                "allowAnonLogging": true,
+                "contentVersion": "1.21.20.0.preview.2",
+                "panelizationSummary": {
+                    "containsEpubBubbles": false,
+                    "containsImageBubbles": false
+                },
+                "imageLinks": {
+                    "smallThumbnail": "http://books.google.com/books/content?id=evuwdDLfAyYC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                    "thumbnail": "http://books.google.com/books/content?id=evuwdDLfAyYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+                },
+                "language": "en",
+                "previewLink": "http://books.google.com/books?id=evuwdDLfAyYC&printsec=frontcover&dq=rowling&hl=&cd=4&source=gbs_api",
+                "infoLink": "https://play.google.com/store/books/details?id=evuwdDLfAyYC&source=gbs_api",
+                "canonicalVolumeLink": "https://market.android.com/details?id=book-evuwdDLfAyYC",
+                "id": "evuwdDLfAyYC",
+                "shelf": "wantToRead"
+            },
+            {
+                "title": "Lords of Finance",
+                "subtitle": "The Bankers Who Broke the World",
+                "authors": [
+                    "Liaquat Ahamed"
+                ],
+                "publisher": "Penguin",
+                "publishedDate": "2009-01",
+                "description": "Argues that the stock market crash of 1929 and subsequent Depression occurred as a result of poor decisions on the part of four central bankers who jointly attempted to reconstruct international finance by reinstating the gold standard.",
+                "industryIdentifiers": [
+                    {
+                        "type": "ISBN_10",
+                        "identifier": "159420182X"
+                    },
+                    {
+                        "type": "ISBN_13",
+                        "identifier": "9781594201820"
+                    }
+                ],
+                "readingModes": {
+                    "text": false,
+                    "image": false
+                },
+                "pageCount": 564,
+                "printType": "BOOK",
+                "categories": [
+                    "Business & Economics"
+                ],
+                "averageRating": 4.5,
+                "ratingsCount": 14,
+                "maturityRating": "NOT_MATURE",
+                "allowAnonLogging": false,
+                "contentVersion": "1.0.0.0.preview.0",
+                "imageLinks": {
+                    "smallThumbnail": "http://books.google.com/books/content?id=74XNzF_al3MC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                    "thumbnail": "http://books.google.com/books/content?id=74XNzF_al3MC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+                },
+                "language": "en",
+                "previewLink": "http://books.google.com/books?id=74XNzF_al3MC&printsec=frontcover&dq=finance&hl=&cd=1&source=gbs_api",
+                "infoLink": "http://books.google.com/books?id=74XNzF_al3MC&dq=finance&hl=&source=gbs_api",
+                "canonicalVolumeLink": "https://books.google.com/books/about/Lords_of_Finance.html?hl=&id=74XNzF_al3MC",
+                "id": "74XNzF_al3MC",
+                "shelf": "wantToRead"
+            }
+          ]
+        }
+  const readBooks = {
+
+                "books" :
+              [
+                {
+                        "title": "Needful Things",
+                        "authors": [
+                            "Stephen King"
+                        ],
+                        "publisher": "Simon and Schuster",
+                        "publishedDate": "2016-05-03",
+                        "description": "Now available for the first time in a mass-market premium paperback edition—master storyteller Stephen King presents the classic #1 New York Times bestseller about a mysterious store than can sell you whatever you desire—but not without exacting a terrible price in return. “There are two prices for this. Half…and half. One half is cash. The other is a deed. Do you understand?” The town of Castle Rock, Maine has seen its fair share of oddities over the years, but nothing is a peculiar as the little curio shop that’s just opened for business. Its mysterious proprietor, Leland Gaunt, seems to have something for everyone out on display at Needful Things…interesting items that run the gamut from worthless to priceless. Nothing has a price tag in this place, but everything is certainly for sale. The heart’s desire for any resident of Castle Rock can easily be found among the curiosities…in exchange for a little money and—at the specific request of Leland Gaunt—a whole lot of menace against their fellow neighbors. Everyone in town seems willing to make a deal at Needful Things, but the devil is in the details. And no one takes heed of the little sign handing on the wall: Caveat emptor. In other words, let the buyer beware…",
+                        "industryIdentifiers": [
+                            {
+                                "type": "ISBN_13",
+                                "identifier": "9781501143786"
+                            },
+                            {
+                                "type": "ISBN_10",
+                                "identifier": "1501143786"
+                            }
+                        ],
+                        "readingModes": {
+                            "text": false,
+                            "image": false
+                        },
+                        "pageCount": 960,
+                        "printType": "BOOK",
+                        "categories": [
+                            "Fiction"
+                        ],
+                        "averageRating": 3.5,
+                        "ratingsCount": 28,
+                        "maturityRating": "NOT_MATURE",
+                        "allowAnonLogging": false,
+                        "contentVersion": "1.3.1.0.preview.0",
+                        "imageLinks": {
+                            "smallThumbnail": "http://books.google.com/books/content?id=jAUODAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                            "thumbnail": "http://books.google.com/books/content?id=jAUODAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+                        },
+                        "language": "en",
+                        "previewLink": "http://books.google.com/books?id=jAUODAAAQBAJ&printsec=frontcover&dq=king&hl=&cd=8&source=gbs_api",
+                        "infoLink": "http://books.google.com/books?id=jAUODAAAQBAJ&dq=king&hl=&source=gbs_api",
+                        "canonicalVolumeLink": "https://books.google.com/books/about/Needful_Things.html?hl=&id=jAUODAAAQBAJ",
+                        "id": "jAUODAAAQBAJ",
+                        "shelf": "read"
+                    }
+          ]
+        }
+
     return (
       <div className="app">
         {this.state.showSearchPage ? (
@@ -140,9 +286,10 @@ class BooksApp extends React.Component {
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
-            <div className="list-books-content">
-              <Shelf books={ wantToReadBooks } />
-            </div>
+
+              <Shelf books={ currentlyReadingBooks } title="Currently Reading" />
+              <Shelf books={ wantToReadBooks } title="Want To Read" />
+              <Shelf books={ readBooks } title="Read" />
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
---
+++ src/components/Shelf.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import '../App.css'
+import Selector from './Selector.js'
+import Books from './Books.js'
+
+class Shelf extends Component {
+
+  render () {
+
+    const { books } = this.props.books
+    const { title } = this.props //added to allow addition of shelfs in a user wants.
+
+    return (
+      <div>
+        <div className="list-books-content">
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">{ title }</h2>
+            <div className="bookshelf-books">
+              <Books books={ books } />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Shelf
